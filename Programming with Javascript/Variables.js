e5dbac4e2806da6edd82036618828a5220a8d53c
dowhatkennydoes <kjdavis2423@gmail.com>
2024-11-11 20:15:39 -0600

    Add Variables file

--- Programming with Javascript/Variables.js
+++ Programming with Javascript/Variables.js
@@ -18,3 +18,29 @@ console.log(name); // Output: Alice
 
 name = "Bob"; // You can change the value of a variable
 console.log(name); // Output: Bob
+
+let score = 95;
+console.log(score); // Output: 95
+
+let greeting = "Hello, World!";
+console.log(greeting); // Output: Hello, World!
+
+let isStudent = true;
+console.log(isStudent); // Output: true
+
+isStudent = false; // Changing the value
+console.log(isStudent); // Output: false
+
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName; // Concatenates firstName and lastName
+console.log(fullName); // Output: John Doe
+
+let x = 10;
+let y = 5;
+
+let sum = x + y; // Adds x and y
+console.log(sum); // Output: 15
+
+let product = x * y; // Multiplies x and y
+console.log(product); // Output: 50
